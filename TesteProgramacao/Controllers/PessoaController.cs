ddd7f86d42de0b100bcbe3f17c64fb41bae7c005
Felipe Antunes de Oliveira <felipe.tunes@outlook.com>
2023-05-11 15:34:23 -0300

    Add project files.

---
+++ TesteProgramacao/Controllers/PessoaController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TesteProgramacao.Models;
+using TesteProgramacao.Repository;
+
+namespace TesteProgramacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+        private PessoaRepository respository = new PessoaRepository();
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            return View(respository.GetAll());
+        }
+
+        // GET: Pessoa/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoa/Create
+        [HttpPost]
+        public ActionResult Create(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                respository.Save(pessoa);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(pessoa);
+            }
+        }
+
+        // GET: Pessoa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var pessoa = respository.GetById(id);
+
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(pessoa);
+        }
+
+        // POST: Pessoa/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                respository.Update(pessoa);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(pessoa);
+            }
+        }
+
+        // POST: Pessoa/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            respository.DeleteById(id);
+            return Json(respository.GetAll());
+        }
+    }
+
+}
---
+++ TesteProgramacao/Models/Pessoa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TesteProgramacao.Models
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Cidade { get; set; }
+
+        public string Endereco { get; set; }
+    }
+}
